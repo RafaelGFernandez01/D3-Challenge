23553a0b6f3b5682502de838b9d5f38071733a25
Rafael Fernandez <rafa7860@yahoo.com>
2021-06-02 20:57:17 -0400

    cleaning files

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// @TODO: YOUR CODE HERE!
+console.log('hello!');
+
+function read_csv_file() {
+    return new Promise((resolve, reject) => {
+        d3.csv("/assets/data/data.csv", (data) => {
+            console.log(data);
+            resolve(data);
+        });
+    });
+}
